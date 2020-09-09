c6ce393f35fbcf7dfef8cb12b654d90df13599c5
ErmineII <65429969+ErmineII@users.noreply.github.com>
2020-09-09 14:40:00 -0400

    initial commit

--- theme.js
+++ theme.js
@@ -1,15 +1,17 @@
-var css  = document.getElementById("theme");
+var css = document.getElementById("theme");
 var icon = document.getElementById("themech");
 
 icon.width = 30;
-icon.height= 30;
-var dtheme = css.href == "dp.css" ? 1 : 0;
-var themes = [["media/moon.svg","khak.css"],
-              ["media/sun.svg", "dp.css"  ]];
+icon.height = 30;
+var dtheme = css.href === "dp.css" ? 1 : 0;
+var themes = [
+  ["media/moon.svg", "khak.css"],
+  ["media/sun.svg", "dp.css"]
+];
 
 icon.src = themes[dtheme][0];
-icon.onclick = ()=>{
-  dtheme   = 1-dtheme;
+icon.onclick = () => {
+  dtheme = 1 - dtheme;
   css.href = themes[dtheme][1];
   icon.src = themes[dtheme][0];
 };
