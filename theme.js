ca2c7d05a06b9eed187d13a8b0b637b5f080aea7
ErmineII <65429969+ErmineII@users.noreply.github.com>
2020-10-02 12:09:44 -0400

    use localStorage for theme choice

--- theme.js
+++ theme.js
@@ -10,16 +10,24 @@ var themes = [
   ["media/snowflake.svg", "all.css"]
 ];
 
-var dtheme = 0;
-for (let i = 0; i < themes.length; i++)
-  if (css.href.match(themes[i][1])) {
-    dtheme = i;
-    break;
-  }
+var dtheme = localStorage.getItem("theme");
+
+if (dtheme === null) {
+  for (let i = 0; i < themes.length; i++)
+    if (css.href.match(themes[i][1])) {
+      dtheme = i;
+      break;
+    }
+} else {
+  css.href = themes[dtheme][1];
+}
+
+dtheme = parseInt(dtheme, 10);
 
 icon.src = themes[(dtheme + 1) % themes.length][0];
 icon.onclick = () => {
   dtheme = (1 + dtheme) % themes.length;
+  localStorage.setItem("theme", dtheme);
   css.href = themes[dtheme][1];
   icon.src = themes[(dtheme + 1) % themes.length][0];
 };
